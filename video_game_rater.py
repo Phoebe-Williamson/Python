a834205e47b1c2f520b4fe68f8d3fb07822d28db
Phoebe <100629391+Pheebs-W@users.noreply.github.com>
2022-04-08 11:44:36 +1200

    Main routine Inital collection
    Added some games with some inital rarings

---
+++ video_game_rater.py
@@ -0,0 +1,9 @@
+##
+# wideo_game_rater.py
+# rates video games
+# 8/4/22
+# Phoebe
+
+if __name__ == "__main__":
+    videogames = {"Minecraft":5, "Call of Duty":1, "Angry Birds":4,
+                  "Splatoon 2":5, "Animal Crossing":4}
