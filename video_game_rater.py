fc2fc8e88e96d6ebc3008c0af80a0877957c903d
Phoebe <100629391+Pheebs-W@users.noreply.github.com>
2022-04-08 11:50:20 +1200

    Added function
    Add a function and printed it

--- video_game_rater.py
+++ video_game_rater.py
@@ -1,9 +1,19 @@
 ##
-# wideo_game_rater.py
+# video_game_rater.py
 # rates video games
 # 8/4/22
 # Phoebe
 
+def print_dictionary(dictionary):
+    """
+    accepts a dictionary, looops through it and prints
+    out the game and its rating
+    """
+    for game, rating in dictionary.items():
+        print("Game: {}\tRating: {}".format(game, rating))
+
+
 if __name__ == "__main__":
     videogames = {"Minecraft":5, "Call of Duty":1, "Angry Birds":4,
                   "Splatoon 2":5, "Animal Crossing":4}
+    print_dictionary(videogames)
