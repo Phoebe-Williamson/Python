ac98ba7d4a5f0b85c594c787fd251111e6ca0ae5
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2022-04-08 12:33:00 +1200

    Album rater plan
    Plan is layed out in code

---
+++ alubum_rater.py
@@ -0,0 +1,26 @@
+##
+# album_rater.py
+# make an album rater program
+# an alubm conatins a title, artist, genre, and rating
+# you should allow the user to add, edit, delete, and rate, and print out all albums
+# when you rate a program, your program should recomend another album
+# album of the same genre
+# you must make this program modular (funtions)
+# you should make version control
+# you SHOULD make this program robust
+##
+# 8/4/22 - ?
+# Authors: Phoebe and Janelle
+
+
+
+
+
+# funtcion for adding album
+# function for deleting album
+# function for editing album
+# function for rating album
+# function for printing album
+# main routine
+# create nested dictionary (dict inside dict)
+# menu #turn into function
