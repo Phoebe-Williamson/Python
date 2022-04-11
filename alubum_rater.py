a214aaca8dce84913621ef69356620f8cc36c37d
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2022-04-11 13:58:41 +1200

    added menu and albums
    Added a menu to ask what funtion they want to do. Added a list of albums with their title, artist, genre, and rating

--- alubum_rater.py
+++ alubum_rater.py
@@ -14,13 +14,63 @@
 
 
 
-
-
 # funtcion for adding album
+def adding_album():
+    pass
 # function for deleting album
+def deleting_album():
+    pass
 # function for editing album
+def editing_albums():
+    pass
 # function for rating album
+def rating_album():
+    pass
 # function for printing album
-# main routine
+def printing_album():
+    pass
+# function for recomending album
+def recomdening_album():
+    pass
 # create nested dictionary (dict inside dict)
+
 # menu #turn into function
+def getting_album():
+    choices = [1, 2, 3, 4, 5, 6]
+    getting_albums = True
+    while getting_albums:
+        try:
+            choice = int(input("\nPlease chose your option\n"
+                           "1: add album\n"
+                           "2: delete album\n"
+                           "3: edit album\n"
+                           "4: rate album\n"
+                           "5: print albums\n"
+                           "6: recomend albums\n"))
+            if choice in choices:
+                print("you have chosen {}.\n".format(choice))
+                getting_albums = False
+        except ValueError:
+            print("enter a valid number")
+        return choice
+                
+
+
+# main routine
+if __name__ == "__main__":
+    albums = {1:{"Title":"Sour",
+                 "Artist":"Olivia Rodrigo",
+                 "Genre":"Pop", "Rating":5},
+              2:{"Title":"Promises",
+                 "Artist":"Sam Shepherd",
+                 "Genre":"Jazz", "Rating":2},
+              3:{"Title":"Sound Ancestors",
+                 "Artist":"Madlib",
+                 "Genre":"Hip-Hop", "Rating":0},
+              4:{"Title":"Valentine",
+                 "Artist":"Snail Mail",
+                 "Genre":"Alternative", "Rating":4},
+              5:{"Title":"Daddy's Home",
+                 "Artist":"St. Vincent",
+                 "Genre":"Pop", "Rating":2}}
+    menu_option = getting_album()
