e647e7ffc5744c9e1e983fe6973774b30738cd81
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2022-05-10 09:22:23 +1200

    Added all the functions
    added the functions and the menu loop so it keeps running through

--- movie_collecter.py
+++ movie_collecter.py
@@ -8,7 +8,7 @@
 # 9/5/22 - 21/5/22
 
 # Done:
-
+# Funtion for menu 
 
 # To do list:
 # Function for adding movie, director and time in minutes
@@ -18,7 +18,7 @@
 
 
 # Funtion for menu 
-def menu_option():
+def getting_movie():
     """
     menu function to allow the user to chose the function to acess
     """
@@ -26,12 +26,13 @@ def menu_option():
     menu_option = True
     while menu_option:
         try:
-            choice = input("Menu\n"
+            choice = input("*** Menu ***"
                            "\n'A': Add movie"
                            "\n'E': Edit movie length"
                            "\n'L': Print out movie length"
                            "\n'P': Print out list of movies"
-                           "\n'Q': Quit")
+                           "\n'Q': Quit"
+                           "\nEnter option here: ")
             if choice in choice:
                 print("You have chosen {}.\n".format(choice))
                 menu_option = False
@@ -42,6 +43,77 @@ def menu_option():
     return choice
 
 
+# function for adding movies
+def add_movie(dictionary):
+    pass
+    """
+
+    """
+
+
+# function for editing a movies length
+def edit_movie(dictionary):
+    pass
+    """
+
+    """
+
+
+# function for printing movie time in hours and minutes
+def movie_length(dictionary):
+    pass
+    """
+
+    """
+
+
+# function for printing out movie list
+def print_movies(dictionary):
+    pass
+    """
+
+    """
+
+
+
+
+
 # main frame
 if __name__ == "__main__":
     
+    movies = {1: {"Title": "Minions: the rise of Gru",
+                  "Director": "Kyle Balda",
+                  "Time in hours": 1, "Time in minutes": 31},
+              2: {"Title": "Uncharted",
+                  "Director": "Ruben Fleischer",
+                  "Time in hours": 1, "Time in minutes": 56},
+              3: {"Title": "The Adam project",
+                  "Director": "Shawn Levy",
+                  "Time in hours": 1, "Time in minutes": 46},
+              4: {"Title": "The power of the dog",
+                  "Director": "Jane Campion",
+                  "Time": 126, " minutes"
+                  "Time in hours": 2, "Time in minutes": 6},
+              5: {"Title": "Rampage",
+                  "Director": "Kyle Balda",
+                  "Time in hours": 1, "Time in minutes": 31}}
+
+# Will loop so menu will keep runing through until killed
+    code_running = True
+    while code_running:
+        menu_option = getting_movie()
+        if menu_option == 'A':
+            add_movie(movies)
+            print_movies(movies)
+        elif menu_option == 'E':
+            print_movies(movies)
+            edit_movie(movies)
+            print_movies(movies)
+        elif menu_option == 'L':
+            movie_length(movies)
+        elif menu_option == 'P':
+            print_movies(movies)
+        elif menu_option == 'Q':
+            code_running = False
+        else:
+            print("That is not a valid option.")
