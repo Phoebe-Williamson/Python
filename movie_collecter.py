0ca0688dcb837f31e8ad105d9a57263f046744b2
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2022-05-09 14:00:19 +1200

    Added plan for code

---
+++ movie_collecter.py
@@ -0,0 +1,26 @@
+##
+# movie_collecter.py
+#
+#
+#
+#
+# Author: Phoebe Williamson
+# 9/5/22 - 21/5/22
+
+# Done:
+
+
+# To do list:
+# Function for adding movie, director and time in minutes
+# Funtion to edit a movie length
+# Function to print out the movie time in hours and minutes
+# Funtion to print out all the movies
+# Funtion for menu 
+
+
+
+
+
+# main frame
+if __name__ == "__main__":
+    
