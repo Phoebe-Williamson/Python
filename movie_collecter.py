015abdd0e8fe1c63d152462b61c78b4163aa2d3b
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2022-05-09 14:16:14 +1200

    Created menu function

--- movie_collecter.py
+++ movie_collecter.py
@@ -1,7 +1,7 @@
 ##
 # movie_collecter.py
-#
-#
+# make a movie collection program
+# 
 #
 #
 # Author: Phoebe Williamson
@@ -15,10 +15,31 @@
 # Funtion to edit a movie length
 # Function to print out the movie time in hours and minutes
 # Funtion to print out all the movies
+
+
 # Funtion for menu 
-
-
-
+def menu_option():
+    """
+    menu function to allow the user to chose the function to acess
+    """
+    choices = ['A', 'E', 'L', 'P', 'Q']
+    menu_option = True
+    while menu_option:
+        try:
+            choice = input("Menu\n"
+                           "\n'A': Add movie"
+                           "\n'E': Edit movie length"
+                           "\n'L': Print out movie length"
+                           "\n'P': Print out list of movies"
+                           "\n'Q': Quit")
+            if choice in choice:
+                print("You have chosen {}.\n".format(choice))
+                menu_option = False
+            else:
+                print("Please enter a valid option, e.g. Q")
+        except ValueError:
+            print("Please enter a valid option, e.g. Q")
+    return choice
 
 
 # main frame
